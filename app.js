21c9ba456059c630f849bd107d91eb74085ece1a
Hedi Naili_Automation <hednailiAutomation@gmail.com>
2025-05-05 22:33:01 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function greet() {
   const name = document.getElementById('name').value;
-  const greeting = `Hello, ${name || 'stranger'}!`;
+  const greeting = `Hi, ${name || 'stranger'}!`;
   document.getElementById('greeting').innerText = greeting;
   return greeting; // return for testing
 }
